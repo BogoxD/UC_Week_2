2fac620174bca9fabec4e4def87aac27da0e3cf2
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-08-05 15:36:03 +0100

    Tank_City

---
+++ Assets/VehicleController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    [Header("Vehicle Movement")]
+    public float Acceleration = 500f;
+    public float BreakForce = 300f;
+    public float MaxTurnAngle = 15f;
+
+    private float currentAcceleration = 0f;
+    private float currentBreakForce = 0f;
+    private float currentTurnAngle = 0f;
+
+    [Header("Vehicle Wheels")]
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
+
+    [SerializeField] Transform frontRightTransform;
+    [SerializeField] Transform frontLeftTransform;
+    [SerializeField] Transform backRightTransform;
+    [SerializeField] Transform backLeftTransform;
+
+    [Header("Ground Check")]
+    [SerializeField] float GroundCheckRadiusSphere = 1f;
+    [SerializeField] LayerMask whatIsGround;
+
+    public bool _grounded;
+    Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        if (Physics.CheckSphere(transform.position, GroundCheckRadiusSphere, whatIsGround))
+            _grounded = true;
+        else
+            _grounded = false;
+
+        if (_grounded)
+        {
+            OnMovement();
+        }
+    }
+    private void OnMovement()
+    {
+        //Get front/reverse and left/right input axis
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        //Apply acceleration
+        currentAcceleration = Acceleration * verticalAxis;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            currentBreakForce = BreakForce;
+        else
+            currentBreakForce = 0f;
+
+        //Apply acceleration to front wheels
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        //Apply break force to all wheels
+        frontRight.brakeTorque = currentBreakForce;
+        frontLeft.brakeTorque = currentBreakForce;
+        backRight.brakeTorque = currentBreakForce;
+        backLeft.brakeTorque = currentBreakForce;
+
+        //Car Steering
+        currentTurnAngle = MaxTurnAngle * horizontalAxis;
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+
+        //Update Wheel Meshes
+        UpdateWheel(frontRight, frontRightTransform);
+        UpdateWheel(frontLeft, frontLeftTransform);
+        UpdateWheel(backRight, backRightTransform);
+        UpdateWheel(backLeft, backLeftTransform);
+    }
+
+    private void UpdateWheel(WheelCollider col, Transform trans)
+    {
+        //Get Wheel Colider State
+        Vector3 position;
+        Quaternion rotation;
+        col.GetWorldPose(out position, out rotation);
+
+        //Set Wheel Transform State
+        trans.position = Vector3.Lerp(trans.position, position, 2f);
+        trans.rotation = Quaternion.Lerp(trans.rotation, rotation, 2f);
+    }
+}
